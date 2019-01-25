b5a9daab86adf8bd824a89452e3e9d64529caffb
jeevan437 <jeevanpjr@gmail.com>
2019-01-25 17:20:01 +0530

    created rest_api

---
+++ Nginix_depApp/Nginix_test/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url,include
+from.views import todo_model
+from .views import index_view
+from rest_framework import routers
+from .viewsets import Todo_ViewSet,DetailsView
+
+router = routers.DefaultRouter()
+
+router.register('api', Todo_ViewSet)
+urlpatterns = [
+
+    url(r'^app/',index_view),   # function based views
+    url(r'^', include(router.urls)),
+    url(r'^todo_list/(?P<pk>[0-9]+)/$',DetailsView.as_view()),
+
+]
